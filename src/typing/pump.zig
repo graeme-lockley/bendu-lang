3be78742ef430d4067a32c3ca13612058289d93b
Graeme Lockley <graeme.lockley@gmail.com>
2024-08-04 15:16:18 +0200

    feature: create an array of new bindings

--- src/typing/pump.zig
+++ src/typing/pump.zig
@@ -21,4 +21,15 @@ pub const Pump = struct {
     pub fn newBound(self: *Pump, allocator: std.mem.Allocator) !*Typing.Type {
         return try Typing.BoundType.new(allocator, self.pump());
     }
+
+    pub fn newBoundN(self: *Pump, allocator: std.mem.Allocator, n: u64) ![]*Typing.Type {
+        var result = try allocator.alloc(*Typing.Type, n);
+        errdefer allocator.free(result);
+
+        for (result, 0..) |_, i| {
+            result[i] = try Typing.BoundType.new(allocator, self.pump());
+        }
+
+        return result;
+    }
 };
