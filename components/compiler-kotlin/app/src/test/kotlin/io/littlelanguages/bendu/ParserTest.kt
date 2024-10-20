cbc86a6ae4e78a6c4b4674ad8feac865390f3841
Graeme Lockley <graeme.lockley@gmail.com>
2024-10-20 13:59:03 +0200

    test: increase the scenarios through parameterising

--- components/compiler-kotlin/app/src/test/kotlin/io/littlelanguages/bendu/ParserTest.kt
+++ components/compiler-kotlin/app/src/test/kotlin/io/littlelanguages/bendu/ParserTest.kt
@@ -7,20 +7,27 @@ import kotlin.test.assertIs
 class ParserTest {
     @Test
     fun parserParses() {
-        val input = "let x = (1); let y = x"
+        listOf(
+            "let x = 1; let y = x",
+            "let x = (1); let y = x",
+            "let x = (((1))); let y = ((x))",
+            "let x = 1 let y = x",
+            "let x = (1) let y = x",
+            "let x = (((1))) let y = ((x))"
+        ).forEach { input ->
+            val statements = parse(input)
 
-        val statements = parse(input)
+            assertEquals(statements.size, 2)
 
-        assertEquals(statements.size, 2)
+            assertIs<LetStatement>(statements[0])
+            assertEquals((statements[0] as LetStatement).id.value, "x")
+            assertIs<LiteralIntExpression>((statements[0] as LetStatement).e)
+            assertEquals(((statements[0] as LetStatement).e as LiteralIntExpression).v.value, 1)
 
-        assertIs<LetStatement>(statements[0])
-        assertEquals((statements[0] as LetStatement).id.value, "x")
-        assertIs<LiteralIntExpression> ((statements[0] as LetStatement).e)
-        assertEquals(((statements[0] as LetStatement).e as LiteralIntExpression).v.value, 1)
-
-        assertIs<LetStatement>(statements[1])
-        assertEquals((statements[1] as LetStatement).id.value, "y")
-        assertIs<LowerIDExpression> ((statements[1] as LetStatement).e)
-        assertEquals(((statements[1] as LetStatement).e as LowerIDExpression).v.value, "x")
+            assertIs<LetStatement>(statements[1])
+            assertEquals((statements[1] as LetStatement).id.value, "y")
+            assertIs<LowerIDExpression>((statements[1] as LetStatement).e)
+            assertEquals(((statements[1] as LetStatement).e as LowerIDExpression).v.value, "x")
+        }
     }
 }
