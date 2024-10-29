4e387dc9dbe1b7dd262edf61a12deb3c23451a7d
Graeme Lockley <graeme.lockley@gmail.com>
2024-10-29 06:11:33 +0200

    feature: interpret add_i32, sub_i32, mul_i32, div_i32, mod_i32 and pow_i32

--- components/bci-zig/src/interpreter.zig
+++ components/bci-zig/src/interpreter.zig
@@ -6,8 +6,6 @@ const Runtime = @import("runtime.zig");
 
 const DEBUG = false;
 
-const stdout = std.io.getStdOut().writer();
-
 pub fn run(bc: []const u8, runtime: *Runtime.Runtime) !void {
     var ip: usize = 0;
     while (ip < bc.len) {
@@ -35,20 +33,62 @@ pub fn run(bc: []const u8, runtime: *Runtime.Runtime) !void {
                 try runtime.push_i32_stack(index);
                 ip += 4;
             },
-            .print_i32 => {
-                const value = runtime.pop();
-
+            .add_i32 => {
                 if (DEBUG) {
+                    std.debug.print("{d}: add_i32\n", .{ip - 1});
+                }
+
+                try runtime.add_i32();
+            },
+            .sub_i32 => {
+                if (DEBUG) {
+                    std.debug.print("{d}: sub_i32\n", .{ip - 1});
+                }
+
+                try runtime.sub_i32();
+            },
+            .mul_i32 => {
+                if (DEBUG) {
+                    std.debug.print("{d}: mul_i32\n", .{ip - 1});
+                }
+
+                try runtime.mul_i32();
+            },
+            .div_i32 => {
+                if (DEBUG) {
+                    std.debug.print("{d}: div_i32\n", .{ip - 1});
+                }
+
+                try runtime.div_i32();
+            },
+            .mod_i32 => {
+                if (DEBUG) {
+                    std.debug.print("{d}: mod_i32\n", .{ip - 1});
+                }
+
+                try runtime.mod_i32();
+            },
+            .pow_i32 => {
+                if (DEBUG) {
+                    std.debug.print("{d}: pow_i32\n", .{ip - 1});
+                }
+
+                try runtime.pow_i32();
+            },
+            .print_i32 => {
+                if (DEBUG) {
+                    const value = runtime.peek();
                     std.debug.print("{d}: print_i32: value={d}\n", .{ ip - 1, Pointer.asInt(value) });
                 }
 
-                try stdout.print("{d}", .{Pointer.asInt(value)});
+                try runtime.print_i32();
             },
             .println => {
                 if (DEBUG) {
                     std.debug.print("{d}: println\n", .{ip - 1});
                 }
-                try stdout.print("\n", .{});
+
+                try runtime.println();
             },
             // else => std.debug.panic("unknown op code: {d}\n", .{op}),
         }
@@ -83,4 +123,76 @@ test "push_i32_stack" {
     try runtime.push_i32_literal(100);
     try run(&bc, &runtime);
     try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 100);
+}
+
+test "add_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.add_i32)};
+    try runtime.push_i32_literal(100);
+    try runtime.push_i32_literal(42);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 142);
+}
+
+test "sub_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.sub_i32)};
+    try runtime.push_i32_literal(100);
+    try runtime.push_i32_literal(42);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 58);
+}
+
+test "mul_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.mul_i32)};
+    try runtime.push_i32_literal(100);
+    try runtime.push_i32_literal(42);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 4200);
+}
+
+test "div_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.div_i32)};
+    try runtime.push_i32_literal(100);
+    try runtime.push_i32_literal(42);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 2);
+}
+
+test "mod_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.mod_i32)};
+    try runtime.push_i32_literal(100);
+    try runtime.push_i32_literal(42);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 16);
+}
+
+test "pow_i32" {
+    var runtime = Runtime.Runtime.init(std.testing.allocator);
+    defer runtime.deinit();
+
+    const bc: [1]u8 = [_]u8{@intFromEnum(Op.pow_i32)};
+    try runtime.push_i32_literal(2);
+    try runtime.push_i32_literal(16);
+    try run(&bc, &runtime);
+
+    try std.testing.expectEqual(Pointer.asInt(runtime.pop()), 65536);
 }
--- components/bci-zig/src/runtime.zig
+++ components/bci-zig/src/runtime.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 
 const Pointer = @import("pointer.zig");
 
+const stdout = std.io.getStdOut().writer();
+
 pub const Runtime = struct {
     allocator: std.mem.Allocator,
     stack: std.ArrayList(Pointer.Pointer),
@@ -28,5 +30,53 @@ pub const Runtime = struct {
     pub inline fn push_i32_stack(self: *Runtime, index: i32) !void {
         const value = self.stack.items[@intCast(index)];
         try self.stack.append(value);
+    }
+
+    pub inline fn add_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(a + b));
+    }
+
+    pub inline fn sub_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(a - b));
+    }
+
+    pub inline fn mul_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(a * b));
+    }
+
+    pub inline fn div_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(@divTrunc(a, b)));
+    }
+
+    pub inline fn mod_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(@mod(a, b)));
+    }
+
+    pub inline fn pow_i32(self: *Runtime) !void {
+        const b = Pointer.asInt(self.pop());
+        const a = Pointer.asInt(self.pop());
+        try self.stack.append(Pointer.fromInt(std.math.pow(i32, a, b)));
+    }
+
+    pub inline fn print_i32(self: *Runtime) !void {
+        const value = Pointer.asInt(self.pop());
+
+        try stdout.print("{d}", .{value});
+    }
+
+    pub inline fn println(self: *Runtime) !void {
+        _ = self;
+
+        try stdout.print("\n", .{});
     }
 };
