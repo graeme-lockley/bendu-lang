852aa16cc3c0b6abec2c265b7494942003ffe6ae
Graeme Lockley <graeme.lockley@gmail.com>
2024-12-18 08:05:37 +0200

    refactor: simplify

--- components/bci-zig/src/runtime.zig
+++ components/bci-zig/src/runtime.zig
@@ -158,20 +158,7 @@ pub const Runtime = struct {
         const start = Pointer.asInt(self.peekN(1));
         const array = Pointer.as(*Memory.Value, self.peekN(2));
 
-        const arrayLen = array.v.ArrayKind.len();
-
-        const aStart: usize = if (start < 0) 0 else if (start >= arrayLen) arrayLen else @intCast(start);
-        const aEnd: usize = if (end < aStart) aStart else if (end >= arrayLen) arrayLen else @intCast(end);
-
-        const range = try self.pushNewValue(Memory.ValueValue{ .ArrayKind = try Memory.ArrayValue.init(self.allocator, aEnd - aStart + 1) });
-
-        for (aStart..aEnd) |i| {
-            const v = array.v.ArrayKind.at(i);
-            if (Pointer.isString(v)) {
-                Pointer.asString(v).incRef();
-            }
-            try range.v.ArrayKind.values.append(v);
-        }
+        const range = try self.push_array_range_from_to(array, start, end);
 
         self.popN(4);
         try self.push(Pointer.fromPointer(*Memory.Value, range));
@@ -181,20 +168,7 @@ pub const Runtime = struct {
         const start = Pointer.asInt(self.peek());
         const array = Pointer.as(*Memory.Value, self.peekN(1));
 
-        const arrayLen = array.v.ArrayKind.len();
-
-        const aStart: usize = if (start < 0) 0 else if (start >= arrayLen) arrayLen else @intCast(start);
-        const aEnd: usize = arrayLen;
-
-        const range = try self.pushNewValue(Memory.ValueValue{ .ArrayKind = try Memory.ArrayValue.init(self.allocator, aEnd - aStart + 1) });
-
-        for (aStart..aEnd) |i| {
-            const v = array.v.ArrayKind.at(i);
-            if (Pointer.isString(v)) {
-                Pointer.asString(v).incRef();
-            }
-            try range.v.ArrayKind.values.append(v);
-        }
+        const range = try self.push_array_range_from_to(array, start, @intCast(array.v.ArrayKind.len()));
 
         self.popN(3);
         try self.push(Pointer.fromPointer(*Memory.Value, range));
@@ -204,9 +178,16 @@ pub const Runtime = struct {
         const end = Pointer.asInt(self.peek());
         const array = Pointer.as(*Memory.Value, self.peekN(1));
 
+        const range = try self.push_array_range_from_to(array, 0, end);
+
+        self.popN(3);
+        try self.push(Pointer.fromPointer(*Memory.Value, range));
+    }
+
+    inline fn push_array_range_from_to(self: *Runtime, array: *Memory.Value, start: i32, end: i32) !*Memory.Value {
         const arrayLen = array.v.ArrayKind.len();
 
-        const aStart: usize = 0;
+        const aStart: usize = if (start < 0) 0 else if (start >= arrayLen) arrayLen else @intCast(start);
         const aEnd: usize = if (end < aStart) aStart else if (end >= arrayLen) arrayLen else @intCast(end);
 
         const range = try self.pushNewValue(Memory.ValueValue{ .ArrayKind = try Memory.ArrayValue.init(self.allocator, aEnd - aStart + 1) });
@@ -219,8 +200,7 @@ pub const Runtime = struct {
             try range.v.ArrayKind.values.append(v);
         }
 
-        self.popN(3);
-        try self.push(Pointer.fromPointer(*Memory.Value, range));
+        return range;
     }
 
     pub inline fn push_bool_true(self: *Runtime) !void {
